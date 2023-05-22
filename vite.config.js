7810c4dea5a1363c5efd99a6d42e672a7569cf2f
Thomas Steiner <tomac@google.com>
2023-05-22 12:16:03 +0200

    Port

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
+    port: 3000,
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',
